44642375091c1bd6e44c947cbcaf40ff320f5056
Md Danish Raza <md.danish0raza@gmail.com>
2026-02-16 13:39:05 +0530

    nextrigh - L-116

--- typescript/src/Trees/questions/nextRight.ts
+++ typescript/src/Trees/questions/nextRight.ts
@@ -1,30 +1,114 @@
+export {};
 //  L-116 populating next right
 
 // Input: root = [1,2,3,4,5,6,7]
 // Output: [1,#,2,3,#,4,5,6,7,#]
 // Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
 
-function nextRight(tree: number[]) {
-  const ans: (string | number)[] = [];
+// implemenation 1
+// with On  - BFS
 
-  for (let i = 0; i < tree.length; i++) {
-    // push the value
-    ans.push(tree[i]);
-    //only pushing the right value # if end of the level
-    if (!checkRight(i, tree)) ans.push("#");
+class node {
+  val: number;
+  left: node | null = null;
+  right: node | null = null;
+  next: node | null = null;
+
+  constructor(val: number) {
+    this.val = val;
   }
-  return ans;
 }
 
-function checkRight(i: number, tree: number[]) {
-  // get parent
-  const parent = (i - 1) / 2;
-  // get right child
-  const right = parent * 2 + 2;
+function connect1(root: node) {
+  if (!root) return null;
 
-  return tree[right] ? true : false;
+  const queue: node[] = [root];
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+
+    for (let i = 0; i < levelSize; i++) {
+      const currentNode = queue.shift()!;
+
+      // if its not the last node then attach its right
+      if (i < levelSize - 1) {
+        currentNode.next = queue[0];
+      }
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+  }
+
+  return root;
 }
 
-const root = [1, 2, 3, 4, 5, 6, 7];
+function print(root: node) {
+  const leftMostQueue: node[] = [root];
+  const ans: (number | string)[] = [];
+  while (leftMostQueue.length) {
+    let current = leftMostQueue.shift();
+    if (current) ans.push(current.val);
+    // add leftmost to queue
+    if (current?.left) leftMostQueue.push(current?.left);
 
-console.log(nextRight(root));
+    // print
+    while (current?.next) {
+      ans.push(current.val);
+      // console.log(current.val);
+      current = current.next;
+    }
+    // level break
+    ans.push("#");
+  }
+  console.log(ans);
+}
+
+const root = new node(1);
+root.left = new node(2);
+root.right = new node(4);
+root.left.left = new node(3);
+root.left.right = new node(5);
+root.right.left = new node(3);
+root.right.right = new node(5);
+
+// console.log(connect1(root));
+// print(root);
+
+// method 2
+// SC - O1
+function connect2(root: node | null): node | null {
+  if (!root) return null;
+
+  // Start at the very top left node
+  let leftmost = root;
+
+  // We traverse down the left side of the tree.
+  // We stop when leftmost.left is null (meaning we hit the bottom level).
+  while (leftmost.left !== null) {
+    // 'current' acts as our iterator across the horizontal level
+    let current: node | null = leftmost;
+
+    while (current !== null) {
+      // 1. Connect the left child to the right child
+      current.left!.next = current.right;
+
+      // 2. Connect the right child to the next parent's left child
+      if (current.next !== null) {
+        current.right!.next = current.next.left;
+      }
+
+      // Move to the next node on this horizontal level
+      current = current.next;
+    }
+
+    // Move down to the next horizontal level
+    leftmost = leftmost.left;
+  }
+
+  //return the original root node
+  return root;
+}
+
+connect2(root);
+print(root);
